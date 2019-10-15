277fc68e5a371026c297694a83667d0c5b355ecb
Tersoo Atsen <tersoo.atsen@outlook.com>
2019-10-15 11:40:16 +0100

    Finish setup backend

---
+++ packages/backend/src/app.js
@@ -0,0 +1,9 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+
+const app = express();
+app.use(bodyParser.json());
+
+app.get('/api/foo', (req, res) => res.json({ foo: 'bar' }));
+
+module.exports = app;
