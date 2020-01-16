9556d370e669bc8bb0bd33dae8a9ff31c2115e31
mrSerious <mrSerious@users.noreply.github.com>
2020-01-16 10:07:44 +0100

    Finish setup redux (#24)

--- packages/frontend/src/index.jsx
+++ packages/frontend/src/index.jsx
@@ -1,14 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter as Router } from 'react-router-dom';
+import { Provider } from 'react-redux';
 
+import store from './store/store';
 import App from './components/app';
 import './index.scss';
 
 const app = (
-  <Router>
-    <App />
-  </Router>
+  <Provider store={store}>
+    <Router>
+      <App />
+    </Router>
+  </Provider>
 );
 
 ReactDOM.render(app, document.getElementById('root'));
---
+++ packages/frontend/src/store/reducers/items.js
@@ -0,0 +1,12 @@
+const initialState = {
+  items: [],
+  singleItem: {},
+  loading: false,
+  error: [],
+};
+
+function itemReducer(state = initialState) {
+  return state;
+}
+
+export default itemReducer;
