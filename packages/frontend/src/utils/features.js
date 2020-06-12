335a09d4cf1991f2d3e5ca73a1e26c0170d3af82
mrSerious <mrSerious@users.noreply.github.com>
2020-06-12 23:30:58 +0100

    finish home page redesign (#37)

--- packages/frontend/src/utils/features.js
+++ packages/frontend/src/utils/features.js
@@ -1,17 +1,28 @@
-import devices from '../assets/images/multiple-devices.svg';
-import activity from '../assets/images/secure.svg';
+import devices from '../assets/images/multiple-devices-100.png';
+import reports from '../assets/images/business-report-100.png';
+import secure from '../assets/images/password-window-100.png';
 
 const features = [{
+  id: 'devices',
   iconSrc: devices,
   altText: 'Multiple devices',
   title: 'Access items anytime',
   desc: 'Manage your inventory on any computer, tablet or phone.',
 },
 {
-  iconSrc: activity,
+  id: 'secure',
+  iconSrc: secure,
   altText: 'Activity tracker',
   title: 'Track changes',
   desc: 'Remain secure by always knowing what’s been changed, when and by whom.',
-}];
+},
+{
+  id: 'reports',
+  iconSrc: reports,
+  altText: 'Reports tracker',
+  title: 'Generate',
+  desc: 'Filter & sort your data to generate CSV and PDF reports.',
+},
+];
 
 export default features;
